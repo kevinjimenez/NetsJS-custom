5fd7f828a094690fdca0bfdf6731539979a65c94
kevinjimenez <kjimenez@29deoctubre.fin.ec>
2020-11-17 09:15:08 -0500

    servicios

--- nest-js/src/app.module.ts
+++ nest-js/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+const mockAppService = {
+  
+}
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: AppService,
+      useValue: mockAppService
+    }
+  ],
 })
 export class AppModule {}
